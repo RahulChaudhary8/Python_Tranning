64b40f60c16bfb582ca87d080c39f847717d3793
RahulChaudhary8 <rahulmahi831@gmail.com>
2024-07-19 10:16:55 +0530

    PrefixSum

---
+++ 15_07_24/FindBinarSearch.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+def FindSubArray(arr,Number):
+
+
+
+
+def main():
+    arr=[3,2,6,7,8,9,12,14,56,-78,-98]
+    queries=[3,3,7,8,2,56,76,876,984,12,34]
+    arr.sort()
+    for e in queries:
+    print(binarySearch(e,arr),end=" ")
+main()
+
