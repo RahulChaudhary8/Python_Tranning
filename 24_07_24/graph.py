97743cc3ec5661f0ebcef8f9db77f3d286cd2144
RahulChaudhary8 <rahulmahi831@gmail.com>
2024-07-24 16:19:53 +0530

    more code

--- 24_07_24/graph.py
+++ 24_07_24/graph.py
@@ -1,4 +1,6 @@
 
+largestPath = ""
+smallestPath = ""
 
 
 def buildGraph(edges,n):
@@ -21,7 +23,6 @@ def printGraph(graph):
 def hasPath(graph,src,des,visited):
     if src == des:
         return True
-
     nbrs = graph[src]
     for i in range(len(nbrs)):
         if nbrs[i] == 1:
@@ -32,18 +33,77 @@ def hasPath(graph,src,des,visited):
                     return True
     return False
 
+def printAllPath(graph,src,des,visited,psf):
+    global largestPath
+    global smallestPath
+    if src == des:
+        if len(psf) > len(largestPath):
+            largestPath = psf
+        if smallestPath == "" or len(psf) < len(smallestPath):
+            smallestPath = psf
+        #print(psf)
+        return
+    nbrs = graph[src]
+    for i in range(len(nbrs)):
+        if nbrs[i] == 1:
+            if visited[i] == 0:
+                visited[i] = 1
+                printAllPath(graph,i,des,visited,
+                    str(src)+"->"+str(i) if psf == "" else psf+"->"+str(i),
+                )
+                visited[i] = 0
+
+def buildGraphWithAdjacencyList(edges,n):
+    graph = [[] for i in range(n)]
+    for edge in edges:
+        src = edge[0]
+        des = edge[1]
+        wt  = edge[2]
+        graph[src].append([src,des,wt])
+        # graph[des].append([des,src,wt])
+    return graph
+
+def printGraphInAdjacencyList(graph):
+    for node in graph:
+        for edges in node:
+            print(edges ,end=" ")
+
+
+def hasPath(graph,src,des,visited):
+    if src == des:
+        return True
+    nbrs = graph[src]
+    for nbr in nbrs:
+        if visited[nbr[1]] == 0:
+            visited[nbr[1]] = 1
+            ans = hasPath(graph,nbr[1],des,visited)
+            if ans == True:
+                return True
+    return False
+    
+
 
 
 
 def main():
-    edges = [[0,1],[0,2],[1,3],[2,3],[4,5],[4,6],[5,7],[6,7]]
+    global smallestPath
+    global largestPath
+    edges = [[0,1],[0,2],[1,3],[3,4],[4,5],[4,6],[5,7],[6,7],[1,5],[2,3]]
     n = 8
     graph = buildGraph(edges,n)
-    #printGraph(graph)
-    src = 3
-    des = 5
+    # #printGraph(graph)
+    src = 0
+    des = 7
     visited = [0 for i in range(n)]
-    ans = hasPath(graph,src,des,visited)
-    print(ans)
+    visited[src] = 1
+    # #ans = hasPath(graph,src,des,visited)
+    # #print(ans)
+    printAllPath(graph,src,des,visited,"")
+    print(smallestPath)
+    print(largestPath)
+    #graph = buildGraphWithAdjacencyList(edges,n)
+    #printGraphInAdjacencyList(graph)
+    #ans = hasPath(graph,src,des,visited)
+    #print(ans)
 
 main()
